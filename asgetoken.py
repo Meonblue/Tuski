1eb547c47ee3ac8e9a4277ae8f4e917bbdf7d960
Meonblue <94110220+Meonblue@users.noreply.github.com>
2023-11-06 18:19:25 +0800

    Update asgetoken.py

--- asgetoken.py
+++ asgetoken.py
@@ -1,54 +1,70 @@
-import asyncio
-import json,re
+import json, re
 import aiohttp
-import aiofiles
-import time,os
+import time, os
+from fake_useragent import UserAgent
+from urllib.parse import unquote_plus
+from session import with_retries
 
-async def getoken(cookie,functionId,body):
-    jd_pin = re.findall('pt_pin=(.*?);', cookie)[0]
+
+@with_retries(max_tries=2, retries_sleep_second=1)
+async def getoken(cookie, functionId, body, proxies, session: aiohttp.ClientSession):
+    jd_pin = unquote_plus(re.findall('pt_pin=(.*?);', cookie)[0])
+    UA = UserAgent().random
     async def get_token():
-        async with aiohttp.ClientSession() as s:
+        try:
             api = "http://192.168.100.8:9090/api/jdsign"
-            sign_data ={
-                "fn":functionId,
-                "body":body
+            sign_data = {
+                "fn": functionId,
+                "body": body
             }
-            response= await s.post(url=api, json=sign_data)
-            # print(response)
-            res_json=await response.json()
+            response = await session.post(url=api, json=sign_data)
+            # print(response.status)
+            res_json = await response.json()
             # print(res_json)
-            url= f'https://api.m.jd.com/client.action?functionId={functionId}&{res_json["body"]}'
+            url = f'https://api.m.jd.com/client.action?functionId={functionId}&{res_json["body"]}'
             # print(url)
             headers = {
-                "User-Agent": "jdapp;android;11.2.8;;;Mozilla/5.0 (Linux; Android 10; ONEPLUS A5010 Build/QKQ1.191014.012; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/76.0.3809.89 MQQBrowser/6.2 TBS/045230 Mobile Safari/537.36",
+                "User-Agent": UA,
                 'Cookie': cookie
             }
-            response=await s.get(url,headers=headers)
-            text_data=await response.text()
-            #print(text_data)
-            token = re.findall('"token":"(.*?)"}$',text_data)[0]
-            #print(token)
+            response = await session.get(url, headers=headers, proxy= proxies)
+            text_data = await response.text()
+            # print(text_data)
+            token = re.findall('"token":"(.*?)"}$', text_data)[0]
+            # print(token)
             return token
+        except Exception as e:
+            return False
 
-    if os.path.exists('cache/tokens.json'):
-        with open("cache/tokens.json", "r", encoding="utf-8") as f:
-            tokens= json.load(f)
-    else:
+    try:
+        if os.path.exists('cache/tokens.json'):
+            with open("cache/tokens.json", "r", encoding="utf-8") as f:
+                tokens = json.load(f)
+        else:
+            tokens = {}
+    except Exception as e:
         tokens = {}
+
     if jd_pin in tokens:
         if tokens[jd_pin]["Expiredtime"] > int(time.time()):
             return tokens[jd_pin]["token"]
         else:
-            token= await get_token()
-            Expired_time= int(time.time()) + 840
-            tokens[jd_pin]= {"token":token, "Expiredtime": Expired_time}
-            async with aiofiles.open("cache/tokens.json", "w", encoding="utf-8") as f:
-                await f.write(json.dumps(tokens, indent=4, ensure_ascii=False))
+            token = await get_token()
+            if token == False:
+                return False
+            Expired_time = int(time.time()) + 840
+            tokens[jd_pin] = {"token": token, "Expiredtime": Expired_time}
+            with open("cache/tokens.json", "w", encoding="utf-8") as f:
+                f.write(json.dumps(tokens, indent=4, ensure_ascii=False))
             return token
+
     else:
         token = await get_token()
+        if token == False:
+            return False
+
         Expiredtime = int(time.time()) + 840
         tokens[jd_pin] = {"token": token, "Expiredtime": Expiredtime}
-        async with aiofiles.open("cache/tokens.json", "w", encoding="utf-8") as f:
-            await f.write(json.dumps(tokens, indent=4, ensure_ascii=False))
+        with open("cache/tokens.json", "w", encoding="utf-8") as f:
+            f.write(json.dumps(tokens, indent=4, ensure_ascii=False))
         return token
